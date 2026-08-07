d5886a4c7f3a175c349c58d764221c5ef15e1bad
belensarome <138254555+belensarome@users.noreply.github.com>
2026-08-07 17:52:57 -0300

    modelado del tablero y agente

--- agente.py
+++ agente.py
@@ -4,9 +4,28 @@ class Agente:
         self.tiene_oro = False
         self.tiene_flecha = True
         self.vivo = True
+        self.direccion_grados = 90
+        self.direccion = "este"
 
     def mover_a(self, x, y):
         self.x, self.y = x, y
 
     def agarrar_oro(self):
         self.tiene_oro = True
+
+    def lanzar_flecha(self):
+        self.tiene_flecha = False
+
+    def girar_derecha(self):
+        self.direccion_grados = (self.direccion_grados + 90) % 360
+        direcciones = { 0: "norte", 90: "este", 180: "sur", 270: "oeste"}
+        self.direccion = direcciones[self.direccion_grados]
+
+    def girar_izquierda(self):
+        self.direccion_grados = (self.direccion_grados - 90) % 360
+        direcciones = { 0: "norte", 90: "este", 180: "sur", 270: "oeste"}
+        self.direccion = direcciones[self.direccion_grados]
+
+    def morir(self):
+        self.vivo = False   
+
--- tablero.py
+++ tablero.py
@@ -2,25 +2,61 @@ import random
 
 TAMANIO = 4
 
+
 def generar_tablero(cant_pozos=2):
-    casillas = [(x, y) for x in range(1, TAMANIO+1)
-    for y in range(1, TAMANIO+1)
-    if (x, y) != (1, 1)]
+    if not 1 <= cant_pozos <= 3:
+        raise ValueError("La cantidad de pozos debe estar entre 1 y 3.")
+
+    casillas = [
+        (x, y)
+        for x in range(1, TAMANIO + 1)
+        for y in range(1, TAMANIO + 1)
+        if (x, y) != (1, 1)
+    ]
+
     random.shuffle(casillas)
+
     wumpus = casillas.pop()
     pozos = [casillas.pop() for _ in range(cant_pozos)]
     oro = casillas.pop()
-    return {'wumpus': wumpus, 'pozos': pozos, 'oro': oro}
+
+    return {
+        "wumpus": wumpus,
+        "pozos": pozos,
+        "oro": oro,
+    }
+
 
 def adyacentes(x, y):
-    candidatas = [(x+1,y), (x-1,y), (x,y+1), (x,y-1)]
-    return [(a, b) for a, b in candidatas
-    if 1 <= a <= TAMANIO and 1 <= b <= TAMANIO]
+    candidatas = [
+        (x + 1, y),
+        (x - 1, y),
+        (x, y + 1),
+        (x, y - 1),
+    ]
+
+    return [
+        (a, b)
+        for a, b in candidatas
+        if 1 <= a <= TAMANIO and 1 <= b <= TAMANIO
+    ]
+
 
 def percibir(tablero, x, y):
+    if not (1 <= x <= TAMANIO and 1 <= y <= TAMANIO):
+        raise ValueError("La posición está fuera del tablero.")
+
     vecinas = adyacentes(x, y)
+
     return {
     'breeze': any(v in tablero['pozos'] for v in vecinas),
     'stench': tablero['wumpus'] in vecinas,
     'glitter': (x, y) == tablero['oro'],
     }
+
+def verificar_peligro(tablero, x, y):
+    if (x, y) == tablero["wumpus"]:
+        return "wumpus"
+    if (x, y) in tablero["pozos"]:
+        return "pozo"
+    return None
